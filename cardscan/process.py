2a8541319586e63c96e1fb62e9ab298dd88e2ac7
Aaron Gunderson <aaron@agundy.com>
2015-05-03 00:21:42 -0400

    Cleaned depencies and removes temp files

--- cardscan/process.py
+++ cardscan/process.py
@@ -5,6 +5,14 @@ import subprocess
 
 import utils
 
+def parseText(text):
+    text = text.strip()
+    # Replace long lines with regular lines
+    text = text.replace('\xe2\x80\x94', '-')
+    # Remove left single quotation mark
+    text = text.replace('\xe2\x80\x98','')
+    return text
+
 def getText(img, regions):
     textChunks = []
     for region in regions:
@@ -19,17 +27,16 @@ def getText(img, regions):
         for line in f:
             print line.strip()
             if line.strip() != '':
-                lines.append(line.strip())
+                lines.append(parseText(line))
         textChunks.append(lines)
-        utils.display([(str(lines), chunk)])
+        # utils.display([(str(lines), chunk)])
         f.close()
+        subprocess.call(['rm', 'tmp.jpg', 'tmp.txt'])
     return textChunks
 
 def getRegions(img):
-    imgray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
-    ret,thresh = cv2.threshold(imgray,0,255,0)
-    edges = cv2.Canny(imgray,150,200,apertureSize = 3) 
-    # Find contours and close
+    grayImg = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    edges = cv2.Canny(grayImg,150,200,apertureSize = 3) 
     kernel = np.ones((3,3),np.uint8)
     edges = cv2.dilate(edges,kernel,iterations = 12)
     binaryImg = cv2.morphologyEx(edges,cv2.MORPH_CLOSE,kernel)
@@ -37,11 +44,11 @@ def getRegions(img):
     # Only take contours of a certain size
     regions = []
     for contour in contours:
-        imgW, imgH, _ = img.shape
+        imgH, imgW, _ = img.shape
         [x, y, w, h] = cv2.boundingRect(contour)
         if w < 50 or h < 50:
             pass
-        elif w > .9*imgW or h > .9*imgH:
+        elif w > .95*imgW or h > .95*imgH:
             pass
         else:
             regions.append((x, y, x+w, y+h))
@@ -54,9 +61,17 @@ def drawRegions(img, regions):
         cv2.rectangle(img, (x1,y1), (x2,y2), (0, 255, 0))
     return img
 
-if __name__ == "__main__":
-    im = cv2.imread('../../stanford_business_cards/scans/011_002.jpg')
-    regions = getRegions(im)
-    utils.display([('', drawRegions(im, regions))])
-    text = getText(im, regions)
+def processCard(img):
+    regions = getRegions(img)
+    text = getText(img, regions)
     print text
+    utils.display([('', drawRegions(img, regions))])
+
+if __name__ == "__main__":
+    imgs = utils.getImages('../../stanford_business_cards/scans/')
+    for img in imgs:
+        processCard(img[1])
+    # im = utils.readImage('../../stanford_business_cards/scans/003_002.jpg')
+    # print im
+    # processCard(im)
+
